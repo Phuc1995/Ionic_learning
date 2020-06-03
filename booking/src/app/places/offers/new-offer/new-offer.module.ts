c9c61f27324d40461e3b9294a475fcc63a970e83
HoangPhuc <Hoangphuc735@gmail.com>
2020-06-03 18:05:15 +0700

    10-17

--- booking/src/app/places/offers/new-offer/new-offer.module.ts
+++ booking/src/app/places/offers/new-offer/new-offer.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { NewOfferPageRoutingModule } from './new-offer-routing.module';
@@ -13,8 +12,9 @@ import { NewOfferPage } from './new-offer.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     NewOfferPageRoutingModule
   ],
   declarations: [NewOfferPage]
 })
-export class NewOfferPageModule {}
+export class NewOfferPageModule { }
